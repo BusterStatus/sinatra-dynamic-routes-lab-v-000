3a22d36915af0e901373375a9484e25b8a5f6795
Matthew Serre <matthew.serre@gmail.com>
2019-06-11 20:12:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @number.times do
-      # string += @phrase
+      string += @phrase
     end
     string
   end
